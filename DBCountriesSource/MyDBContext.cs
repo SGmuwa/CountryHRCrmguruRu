0ed243cb16c2aeb7dc63ced109f321134362847a
SG_Muwa <motherlode.muwa@gmail.com>
2020-06-18 19:18:25 +0300

    DBCountriesSource: Create MyDBContext and GetterCountries

---
+++ DBCountriesSource/MyDBContext.cs
@@ -0,0 +1,37 @@
+/*
+    CountryHRCrmguruRu.
+    Copyright (C) 2020
+    Mikhail Pavlovich Sidorenko (motherlode.muwa@gmail.com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using System.Diagnostics.CodeAnalysis;
+using DBCountriesSource.Tables;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBCountriesSource
+{
+    class MyDBContext : DbContext
+    {
+        private readonly DbContextOptions options;
+
+        public MyDBContext([NotNull] DbContextOptions options) : base(options)
+            => this.options = options;
+
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Region> Regions { get; set; }
+    }
+}
