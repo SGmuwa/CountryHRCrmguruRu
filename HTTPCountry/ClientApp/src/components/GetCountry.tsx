21a9c2503b583d2b3984b650ef50b94062786cb9
SG_Muwa <motherlode.muwa@gmail.com>
2020-06-20 17:23:11 +0300

    HTTPCountry: mock Home → mock Fetch

--- HTTPCountry/ClientApp/src/components/GetCountry.tsx
+++ HTTPCountry/ClientApp/src/components/GetCountry.tsx
@@ -1,23 +1,84 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { RouteComponentProps } from 'react-router';
+import { Link } from 'react-router-dom';
+import { ApplicationState } from '../store';
+import * as WeatherForecastsStore from '../store/WeatherForecasts';
 
-const GetCountry = () => (
-  <div>
-    <h1>Hello, world!</h1>
-    <p>Welcome to your new single-page application, built with:</p>
-    <ul>
-      <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-      <li><a href='https://facebook.github.io/react/'>React</a> and <a href='https://redux.js.org/'>Redux</a> for client-side code</li>
-      <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-    </ul>
-    <p>To help you get started, we've also set up:</p>
-    <ul>
-      <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-      <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-      <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-    </ul>
-    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-  </div>
-);
+// At runtime, Redux will merge together...
+type WeatherForecastProps =
+  WeatherForecastsStore.WeatherForecastsState // ... state we've requested from the Redux store
+  & typeof WeatherForecastsStore.actionCreators // ... plus action creators we've requested
+  & RouteComponentProps<{ startDateIndex: string }>; // ... plus incoming routing parameters
 
-export default connect()(GetCountry);
+
+class GetCountry extends React.PureComponent<WeatherForecastProps> {
+  // This method is called when the component is first added to the document
+  public componentDidMount() {
+    this.ensureDataFetched();
+  }
+
+  // This method is called when the route parameters change
+  public componentDidUpdate() {
+    this.ensureDataFetched();
+  }
+
+  public render() {
+    return (
+      <React.Fragment>
+        <h1 id="tabelLabel">Weather forecast</h1>
+        <p>This component demonstrates fetching data from the server and working with URL parameters.</p>
+        {this.renderForecastsTable()}
+        {this.renderPagination()}
+      </React.Fragment>
+    );
+  }
+
+  private ensureDataFetched() {
+    const startDateIndex = parseInt(this.props.match.params.startDateIndex, 10) || 0;
+    this.props.requestWeatherForecasts(startDateIndex);
+  }
+
+  private renderForecastsTable() {
+    return (
+      <table className='table table-striped' aria-labelledby="tabelLabel">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Temp. (C)</th>
+            <th>Temp. (F)</th>
+            <th>Summary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.forecasts.map((forecast: WeatherForecastsStore.WeatherForecast) =>
+            <tr key={forecast.date}>
+              <td>{forecast.date}</td>
+              <td>{forecast.temperatureC}</td>
+              <td>{forecast.temperatureF}</td>
+              <td>{forecast.summary}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    );
+  }
+
+  private renderPagination() {
+    const prevStartDateIndex = (this.props.startDateIndex || 0) - 5;
+    const nextStartDateIndex = (this.props.startDateIndex || 0) + 5;
+
+    return (
+      <div className="d-flex justify-content-between">
+        <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${prevStartDateIndex}`}>Previous</Link>
+        {this.props.isLoading && <span>Loading...</span>}
+        <Link className='btn btn-outline-secondary btn-sm' to={`/fetch-data/${nextStartDateIndex}`}>Next</Link>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  (state: ApplicationState) => state.weatherForecasts, // Selects which state properties are merged into the component's props
+  WeatherForecastsStore.actionCreators // Selects which action creators are merged into the component's props
+)(GetCountry as any);
