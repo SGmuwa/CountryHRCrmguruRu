ee379cfc04c1a104b3dcd118dff5badb3b7f5415
SG_Muwa <motherlode.muwa@gmail.com>
2020-06-20 17:13:53 +0300

    HTTPCountry: Create simple GetCountry page

--- HTTPCountry/ClientApp/src/App.tsx
+++ HTTPCountry/ClientApp/src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Route } from 'react-router';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import GetCountry from './components/GetCountry';
 import Counter from './components/Counter';
 import FetchData from './components/FetchData';
 
@@ -12,5 +13,6 @@ export default () => (
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
+        <Route path='/get-country' component={GetCountry} />
     </Layout>
 );
